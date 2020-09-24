2a6ebd2d1dd7822d839e47d441936fa8c42973f3
Ashish Karn <ashishkrkarn@gmail.com>
2020-09-24 09:02:52 +0530

    code updated

--- src/main/java/karn/deloitte/shortenurls/controllers/URLController.java
+++ src/main/java/karn/deloitte/shortenurls/controllers/URLController.java
@@ -11,15 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.websocket.server.PathParam;
-import java.net.URI;
 
-
-//http://localhost:8080/addURL
 
 /**
  * Controller class to handle requests
@@ -62,7 +57,7 @@ public class URLController {
         LOGGER.info("shortUrl:>>{}",shortUrl);
         String url = iurlProcessorService.getOriginalUrl(shortUrl);
         if (url == null)
-            ResponseEntity.noContent().build();
+           return ResponseEntity.noContent().build();
         return ResponseEntity.status(HttpStatus.FOUND).body(url);
     }
 
